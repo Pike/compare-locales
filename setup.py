9fb349469c88e7af552fc5be34e7dc502bf8c04f
Axel Hecht <axel@pike.org>
2017-07-18 13:53:40 +0200

    prepare for next release, 2.0.1, de-beta, too
    It's been a while since compare-locales is out of beta,
    finally declare it so.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 from compare_locales import version
 
 CLASSIFIERS = """\
-Development Status :: 4 - Beta
+Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)
 Operating System :: OS Independent
