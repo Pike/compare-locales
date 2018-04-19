55a8a98e7f43b680c385773d10ee286b645e08ea
Axel Hecht <axel@pike.org>
2018-04-19 22:26:38 +0200

    bug 1455318, parser.PropertiesParser -> parser.properties.PropertiesParser, rs=stas

--- compare_locales/parser/__init__.py
+++ compare_locales/parser/__init__.py
@@ -17,13 +17,16 @@ from .dtd import (
 from .fluent import (
     FluentParser, FluentComment, FluentEntity, FluentMessage, FluentTerm,
 )
-from six import unichr
+from .properties import (
+    PropertiesParser, PropertiesEntity
+)
 
 __all__ = [
     "CAN_NONE", "CAN_COPY", "CAN_SKIP", "CAN_MERGE",
-    "Comment",
+    "Entity", "Comment",
     "DTDEntity",
     "FluentComment", "FluentEntity", "FluentMessage", "FluentTerm",
+    "PropertiesEntity",
 ]
 
 __constructors = []
@@ -34,84 +37,6 @@ def getParser(path):
         if re.search(item[0], path):
             return item[1]
     raise UserWarning("Cannot find Parser")
-
-
-class PropertiesEntity(Entity):
-    escape = re.compile(r'\\((?P<uni>u[0-9a-fA-F]{1,4})|'
-                        '(?P<nl>\n[ \t]*)|(?P<single>.))', re.M)
-    known_escapes = {'n': '\n', 'r': '\r', 't': '\t', '\\': '\\'}
-
-    @property
-    def val(self):
-        def unescape(m):
-            found = m.groupdict()
-            if found['uni']:
-                return unichr(int(found['uni'][1:], 16))
-            if found['nl']:
-                return ''
-            return self.known_escapes.get(found['single'], found['single'])
-
-        return self.escape.sub(unescape, self.raw_val)
-
-
-class PropertiesParser(Parser):
-
-    Comment = OffsetComment
-
-    def __init__(self):
-        self.reKey = re.compile(
-            '(?P<key>[^#! \t\r\n][^=:\n]*?)[ \t]*[:=][ \t]*', re.M)
-        self.reComment = re.compile('(?:[#!][^\n]*\n)*(?:[#!][^\n]*)', re.M)
-        self._escapedEnd = re.compile(r'\\+$')
-        self._trailingWS = re.compile(r'[ \t\r\n]*(?:\n|\Z)', re.M)
-        Parser.__init__(self)
-
-    def getNext(self, ctx, offset):
-        # overwritten to parse values line by line
-        contents = ctx.contents
-
-        m = self.reWhitespace.match(contents, offset)
-        if m:
-            return Whitespace(ctx, m.span())
-
-        m = self.reComment.match(contents, offset)
-        if m:
-            self.last_comment = self.Comment(ctx, m.span())
-            return self.last_comment
-
-        m = self.reKey.match(contents, offset)
-        if m:
-            startline = offset = m.end()
-            while True:
-                endval = nextline = contents.find('\n', offset)
-                if nextline == -1:
-                    endval = offset = len(contents)
-                    break
-                # is newline escaped?
-                _e = self._escapedEnd.search(contents, offset, nextline)
-                offset = nextline + 1
-                if _e is None:
-                    break
-                # backslashes at end of line, if 2*n, not escaped
-                if len(_e.group()) % 2 == 0:
-                    break
-                startline = offset
-
-            # strip trailing whitespace
-            ws = self._trailingWS.search(contents, startline)
-            if ws:
-                endval = ws.start()
-
-            pre_comment = self.last_comment
-            self.last_comment = None
-            entity = PropertiesEntity(
-                ctx, pre_comment,
-                (m.start(), endval),   # full span
-                m.span('key'),
-                (m.end(), endval))   # value span
-            return entity
-
-        return self.getJunk(ctx, offset, self.reKey, self.reComment)
 
 
 class DefinesInstruction(EntityBase):
