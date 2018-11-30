ce79251fabdb3d5d97291ee1c74a43834b81b811
Axel Hecht <axel@pike.org>
2018-11-30 12:39:25 +0100

    check-en-US script not used anymore, superseded by --validate

--- compare_locales/compare/content.py
+++ compare_locales/compare/content.py
@@ -142,6 +142,7 @@ class ContentComparer:
                 parser.CAN_COPY, None)
             return
         try:
+            import pdb; pdb.set_trace()
             p.readContents(ref_file.getContents())
         except Exception as e:
             self.observers.notify('error', ref_file, str(e))
